Fix EMA calculation for window tennor

algorithm/stat_methods.py
@@ -3,11 +3,11 @@ import numpy as np
 import math
 
 
-def calc_log_returns(levels: np.ndarray):
+def calc_log_returns(levels: np.ndarray, window_size: int = 1):
     n = len(levels)
-    levels_t_minus1 = np.zeros(n)
-    levels_t_minus1[1:] = levels[:-1]
-    return np.log((levels / levels_t_minus1)[1:])
+    levels_t_minus_s = np.zeros(n)
+    levels_t_minus_s[window_size:] = levels[:-window_size]
+    return np.log((levels / levels_t_minus_s)[window_size:])
 
 
 def calc_annual_realised_vol(levels: np.ndarray) -> np.ndarray:
@@ -69,21 +69,17 @@ def calc_moving_percentile(arr: np.ndarray, window_size: int) -> np.ndarray:
     return output
 
 
-def forecast_vol(levels: np.ndarray, model_name: str, **params) -> np.ndarray:
-    # TODO: Review implementation.
-    if model_name.lower() == "ema":
-        log_returns = calc_log_returns(levels)
-        λ = params["l"]
-        σ_0 = params["vol_0"]
-        ema_vols = np.zeros(len(levels))
-        ema_vols[0] = σ_0
-        for i, r in enumerate(log_returns):
-            ema_vols[i + 1] = (λ * ema_vols[i] ** 2 + (1 - λ) * r ** 2) ** (0.5)
-        return ema_vols
-
-    #  Implement further vol forecast models ...
-    else:
-        raise ValueError(f"Unkown model_name '{model_name}'.")
+def forecast_ema_vol(
+    levels: np.ndarray, vol_0: float, window_size: int = 1, _lambda: float = 0.9
+) -> np.ndarray:
+    log_returns = calc_log_returns(levels, window_size)
+    σ_0 = vol_0
+    λ = _lambda
+    ema_vols = np.zeros(len(levels))
+    ema_vols[0] = σ_0
+    for i, r in enumerate(log_returns):
+        ema_vols[i + 1] = (λ * ema_vols[i] ** 2 + (1 - λ) * r ** 2) ** (0.5)
+    return ema_vols
 
 
 def gridiserFactory(shape: tuple) -> callable:

notebook.py
@@ -11,7 +11,7 @@ from algorithm.utils import (
 from algorithm.stat_methods import (
     calc_annual_realised_vol,
     calc_moving_percentile,
-    forecast_vol,
+    forecast_ema_vol,
 )
 from algorithm.trade_classes import VarianceSwap
 from algorithm.graphics import PandasHeatMapPlot
@@ -20,13 +20,13 @@ import numpy as np
 from datetime import timedelta
 
 not_nan = lambda val: not np.isnan(val)
-YEAR_WINDOW = 252 # 1Y (in business_days)
+YEAR_WINDOW = 252  # 1Y (in business_days)
 
 
 #%%
 # Imputs
 swap_window_size = 21  # 1M (in business_days)
-percentile_window_size = YEAR_WINDOW  
+percentile_window_size = YEAR_WINDOW
 ema_lambda = 0.97
 
 x_cells_in_plot = 20
@@ -64,15 +64,21 @@ print(df.head())
 
 # Calculate forecasted 1M realised vol using EMA
 start = get_index_of_first(df["spot"], not_nan)
-first_year_spots = np.array(df["spot"].iloc[start:start + YEAR_WINDOW])
-vol_0_monthly = calc_annual_realised_vol(first_year_spots) / (12 ** 0.5) # From annualised to monthly
+first_year_spots = np.array(df["spot"].iloc[start : start + YEAR_WINDOW])
+vol_0_monthly = calc_annual_realised_vol(first_year_spots) / (
+    12 ** 0.5
+)  # From annualised to monthly
 
 df["1m_realised_ema_vol_forecast"] = np.NaN
-df["1m_realised_ema_vol_forecast"][start:] = forecast_vol(
-    np.array(df["spot"].iloc[start:]), "ema", vol_0=vol_0_monthly, l=ema_lambda
+df["1m_realised_ema_vol_forecast"][start:] = forecast_ema_vol(
+    levels=np.array(df["spot"].iloc[start:]),
+    vol_0=vol_0_monthly,
+    window_size=swap_window_size,
+    _lambda=ema_lambda
 )
 print(df.head())
 
+#%%
 # Remove null values araising from window discrepancies
 df.dropna(inplace=True)
 print(df.head())
@@ -139,11 +145,6 @@ df["profitable"] = df["payoff"] > 0
 #%%
 # Plot heatmap
 plot_cols = ["1y_implied_vol_percentile", "vol_carry", "profitable"]
-plot = PandasHeatMapPlot(
-    df[plot_cols],
-    x_cells_in_plot,
-    y_cells_in_plot,
-    *plot_cols
-)
+plot = PandasHeatMapPlot(df[plot_cols], x_cells_in_plot, y_cells_in_plot, *plot_cols)
 plot.show()
 # %%

tests/test_stat_methods.py
@@ -3,7 +3,7 @@ from algorithm.stat_methods import (
     sum_squares_moving_window,
     calc_moving_annual_realised_vol,
     calc_moving_percentile,
-    forecast_vol,
+    forecast_ema_vol,
     gridiserFactory,
 )
 
@@ -44,7 +44,7 @@ def test_calc_moving_perentile():
 
 def test_ema_forecast():
     vol_0 = 0.210
-    results = forecast_vol(test_data.dummy_levels, "ema", vol_0=vol_0, l=0.9)
+    results = forecast_ema_vol(test_data.dummy_levels, vol_0=vol_0, _lambda=0.9)
     assert all(np.round(results, 3) == test_data.expected_ema_forecast)
 
 